e5fffa7c5d3baa6e83fbed133a68e78f0718310c
npnorman <npn.not@gmail.com>
2025-05-23 01:08:19 -0400

    testing guass

--- blur.py
+++ blur.py
@@ -77,24 +77,50 @@ def createUniformKernel(rowSize,colSize):
 
     return np.full((rowSize,colSize, 3), 1/(rowSize*colSize))
 
+def gaussianKernel(size, sigma):
+    # https://www.kaggle.com/code/dasmehdixtr/gaussian-filter-implementation-from-scratch
+    #based on guass' funtion
+
+    ax = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
+    xx, yy = np.meshgrid(ax, ax)
+
+    kernel = np.exp(-0.5 * (np.square(xx) + np.square(yy)) / np.square(sigma))
+
+    #kernel = np.repeat(kernel[:,:,np.newaxis], 3, axis=2)
+
+    print(kernel/ np.sum(kernel))
+    print("opencv\n",cv2.getGaussianKernel(size,sigma))
+
+    return kernel / np.sum(kernel)
+
 if __name__ == "__main__":
     #img = loadImage("images/number_zero.jpg")
     [img, img_original] = loadImage("images/dog_photo.jpg")
+    [gimg, gimg_original] = loadImage("images/dog_photo.jpg")
+
     #define a kernel
     #box blur
     kernel = createUniformKernel(21,21)
 
     #gaussian blur
+    gKernel = gaussianKernel(5,100)
 
     #process image
     processImage(kernel, img, img_original)
+    processImage(gKernel, gimg, gimg_original)
 
     fig = plt.figure(figsize=(15,8))
-    plt.subplot(1,2,1)
+    plt.subplot(2,2,1)
     plt.imshow(img_original[:,:,::-1])
 
-    plt.subplot(1,2,2)
+    plt.subplot(2,2,2)
     plt.imshow(img[:,:,::-1])
+
+    plt.subplot(2,2,3)
+    plt.imshow(gimg_original[:,:,::-1])
+
+    plt.subplot(2,2,4)
+    plt.imshow(cv2.GaussianBlur(gimg, (21,21), 100))
 
     plt.show()
     #showImage(img)
