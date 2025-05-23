6461c0ce298604a6bf4e0793fd943369e1218a37
npnorman <npn.not@gmail.com>
2025-05-22 22:37:43 -0400

    Added opencv image and uniform kernel

--- blur.py
+++ blur.py
@@ -2,3 +2,31 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+
+def loadImage(image_path):
+    img = cv2.imread(image_path)
+
+    #return arrays
+    return img
+
+def showImage(img):
+    plt.imshow(img[:,:,::-1])
+    plt.show()
+
+def processImage(img):
+    #define a
+    pass
+
+def createUniformKernel(rowSize,colSize):
+    #creates a uniform array
+    return np.full((rowSize,colSize), 1/(rowSize*colSize))
+
+if __name__ == "__main__":
+    img = loadImage("images/number_zero.jpg")
+
+    #define a kernel
+    kernel = createUniformKernel(3,3)
+    print(kernel)
+    #process image
+
+    showImage(img)
